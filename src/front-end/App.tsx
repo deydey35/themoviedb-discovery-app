e3ccebc7787ddad875f97d21aff0d71543489cc6
deydey35 <117529398+deydey35@users.noreply.github.com>
2026-09-21 16:12:44 +0000

    feat: ✨ ajouter le balisage sémantique html

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,10 +1,24 @@
 import { useEffect, useState } from 'react';
 import type { Movie } from '../back-end/schemas/MoviesTypes';
 import MovieItem from './components/MovieItem';
-import './global.css';
+<<<<<<< Updated upstream
 import './app.css';
 
 export default function App() {
+  useEffect(() => {
+    fetch('/api/movies/popular')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+=======
+import './global.css'
+import './app.css';
+
+export default function App() {
+>>>>>>> Stashed changes
   const [movies, setMovies] = useState<Movie[] | null>(null);
 
   useEffect(() => {
@@ -16,12 +30,39 @@ export default function App() {
   }, []);
 
   return (
+<<<<<<< Updated upstream
+    <div>
+      <main className="app-shell">
+        <header className="app-header">
+          <h1>Films populaires</h1>
+          <h2>
+            Films tendances en France, d'après les données de{' '}
+            <b>The Movie Database</b>
+          </h2>
+        </header>
+        <section>
+          {movies ? (
+            <ul className="movie-grid">
+              {movies.map((movie) => (
+                <li key={movie.id}>
+                  <article>
+                    <MovieItem movie={movie} />
+                  </article>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="status-message">Loading...</p>
+          )}
+        </section>
+      </main>
+    </div>
+=======
     <main className="app-shell">
       <header className="app-header">
         <h1>Films populaires</h1>
         <h2>
-          Films tendances en France, d'après les données de{' '}
-          <b>The Movie Database</b>
+          Films tendances en France, d'après les données de <b>The Movie Database</b>
         </h2>
       </header>
       <section>
@@ -40,5 +81,6 @@ export default function App() {
         )}
       </section>
     </main>
+>>>>>>> Stashed changes
   );
 }
--- src/front-end/main.tsx
+++ src/front-end/main.tsx
@@ -1,10 +1,21 @@
+<<<<<<< Updated upstream
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './global.css';
 import App from './App.tsx';
+=======
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./global.css";
+>>>>>>> Stashed changes
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
+<<<<<<< Updated upstream
 );
+=======
+);
+>>>>>>> Stashed changes
