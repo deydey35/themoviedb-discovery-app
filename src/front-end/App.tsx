23d759befb48954ac02cf553dbb1f803e6bc23d5
deydey35 <117529398+deydey35@users.noreply.github.com>
2026-09-23 15:34:17 +0000

    feat: ✨ ajouter la gestion des paramètres de requête pour le fetching des films

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react';
 import type { Movie } from '../back-end/schemas/MoviesTypes';
+import { DEFAULT_LANGUAGE, DEFAULT_PAGE, DEFAULT_REGION } from '../back-end/constants';
 import MovieItem from './components/MovieItem';
 
 export default function App() {
-  useEffect(() => {
-    // fetch data from an API /api/movies/popular
-    fetch('/api/movies/popular')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }, []);
-
   const [movies, setMovies] = useState<Movie[] | null>(null);
-
+  const queryParams = new URLSearchParams(window.location.search);
+  const language = queryParams.get('language') || DEFAULT_LANGUAGE;
+  const page = queryParams.get('page') || DEFAULT_PAGE;
+  const region = queryParams.get('region') || DEFAULT_REGION;
+  const title = language.startsWith('fr')
+    ? 'Films Populaires'
+    : 'Popular Movies';
   // useEffect hook to fetch data from an API when the component mounts
   useEffect(() => {
+
+    // 2. Construire la query string pour le back-end
+    const apiParams = new URLSearchParams({
+      language,
+      page,
+      region,
+    });
     // fetch data from an API /api/movies/popular
-    fetch('/api/movies/popular')
+    fetch(`/api/movies/popular?language=${language}&page=${page}&region=${region}`)
       .then((response) => response.json())
       .then((data) => {
         console.log('Fetched movies data:', data); // Log the fetched data for debugging
@@ -27,7 +32,7 @@ export default function App() {
 
   return (
     <div>
-      <h1>Popular Movies</h1>
+      <h1>{title}</h1>
       {movies ? (
         <ul>
           {movies.map((movie) => (
